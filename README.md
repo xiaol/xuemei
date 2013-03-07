7b262ffa747049638367b0d9f59f9bcadcd1cc5e
ubuntu <ubuntu@10-4-3-161.(none)>
2013-03-07 14:06:59 +0800

    config postgresql and rest framework

--- README.md
+++ README.md
@@ -2,3 +2,25 @@ xuemei
 ======
 
 Web server for image host
+
+install django 1.5rc
+
+Todo: install django restful framework. test it.
+
+pip install djangorestframework 
+
+$sudo apt-get update
+
+$sudo apt-get install postgresql
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
+dpkg-reconfigure locales
+
+sudo -u postgres createuser --superuser <someuser> -P
+
+sudo vi /etc/postgresql/9.1/main/pg_hba.conf
+
+local all all md5
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,13 +11,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'xuemeidb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'xuemei',
+        'PASSWORD': '123,./xl4xue',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'rest_framework',
 )
 
 # A sample logging configuration. The only tangible logging
