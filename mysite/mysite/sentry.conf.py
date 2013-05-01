892c4c5b5dced0283ca634bfc6316d3c8bec6832
ubuntu <ubuntu@ishoow.cn>
2013-05-01 18:07:41 +0800

    hybrid userena and rest framework

--- mysite/mysite/sentry.conf.py
+++ mysite/mysite/sentry.conf.py
@@ -72,9 +72,9 @@ SENTRY_WEB_OPTIONS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'localhost'
-EMAIL_HOST_PASSWORD = ''
-EMAIL_HOST_USER = ''
-EMAIL_PORT = 25
+#EMAIL_HOST_PASSWORD = ''
+#EMAIL_HOST_USER = ''
+EMAIL_PORT = 25 
 EMAIL_USE_TLS = False
 
 # http://twitter.com/apps/new
